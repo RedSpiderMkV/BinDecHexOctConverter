9bc2afc59e6d197f64bffa182e4cc24a406773cb
RedSpiderMkV <redspidermkv@hotmail.com>
2016-01-07 22:39:36 +0000

    output rename

--- src/BinDecHexConverter/nbproject/Makefile-variables.mk
+++ src/BinDecHexConverter/nbproject/Makefile-variables.mk
@@ -9,8 +9,8 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux
 CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
-CND_ARTIFACT_NAME_Debug=bdhconverter
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/bdhconverter
+CND_ARTIFACT_NAME_Debug=bdhconvert
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/bdhconvert
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=bindechexconverter.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/bindechexconverter.tar
